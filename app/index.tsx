e06e4d38a6eb9f81d173637cb5872af08c720f20
papyntown <realistedem@gmail.com>
2024-03-21 14:31:13 +0100

    save

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,80 @@
 import { Stack, Link } from 'expo-router';
-import { Text, View, XStack, Image } from 'tamagui';
+import { Image as LucideImg } from 'lucide-react-native';
+import { Text, View, XStack, Image, Switch, Button, Sheet } from 'tamagui';
 
-import { Container, Main, Title, Subtitle, Button, ButtonText } from '../tamagui.config';
+import { Container, Main, Title, Subtitle, ButtonText } from '../tamagui.config';
 import { StatusBar } from 'expo-status-bar';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 const PlaceholderImage = require('../assets/images/background-image.png');
 
 export default function Page() {
+  const [selectedImage, setSelectedImage] = useState(PlaceholderImage);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const [open, setOpen] = useState(false);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
     <View flex={1} backgroundColor={'$background'} alignItems="center">
-      <View flex={1} justifyContent="center" alignItems="center">
+      <View
+        flex={3 / 4}
+        width={'100%'}
+        justifyContent="center"
+        alignItems="center"
+        backgroundColor={'$blue10'}>
         <Image
           source={{
-            uri: PlaceholderImage,
+            uri: selectedImage,
             width: 320,
             height: 440,
           }}
           borderRadius={18}
+          width={'80%'}
         />
       </View>
-      <StatusBar style="auto" />
+      <View
+        display="flex"
+        flex={1 / 4}
+        width={'100%'}
+        flexDirection="column"
+        gap="$2"
+        alignItems="center"
+        backgroundColor={'$color.red10Dark'}>
+        <Button
+          backgroundColor={'white'}
+          color={'black'}
+          size={'$6'}
+          width={'80%'}
+          borderColor={'$color.red10Dark'}
+          onPress={() => setShowAppOptions(true)}>
+          {' '}
+          <LucideImg color={'black'} />
+          Use this photo
+        </Button>
+        <Button size={'$6'} borderColor={'white'} width={'80%'} onPress={pickImageAsync}>
+          Chose a photo
+        </Button>
+        <Button size={'$6'} borderColor={'white'} width={'80%'} onPress={() => setOpen(true)}>
+          OPenz
+        </Button>
+      </View>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <Sheet.Overlay />
+        <Sheet.Handle />
+        <Sheet.Frame>{<Text>Content</Text>}</Sheet.Frame>
+      </Sheet>
+      <StatusBar style="light" />
     </View>
   );
 }
